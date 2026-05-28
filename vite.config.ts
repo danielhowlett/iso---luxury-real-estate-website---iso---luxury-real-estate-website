02dd1b7eec72b1ad848a6d6f12fb88c417eb3124
danielhowlett <121407310+danielhowlett@users.noreply.github.com>
2026-05-28 18:30:55 +0000

    Update site for Cloudflare Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,11 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig(() => ({
   plugins: [react()],
-  publicDir: "./static",
-  base: "./",
+  publicDir: "static",
+  base: "/",
+  build: {
+    outDir: "dist",
+  },
   server: {
     host: "0.0.0.0",
     port: 5000,
